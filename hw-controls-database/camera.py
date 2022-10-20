b2297d43ef6ba61bd717ba74e268be73c2ae87ca
Stefan Todorovic <stefantodorovic@gmail.com>
2022-10-19 21:31:43 -0400

    created basic camera class

--- hw-controls-database/camera.py
+++ hw-controls-database/camera.py
@@ -1,20 +1,43 @@
-import cv2 as cv
-import numpy as np
+import cv2
 
-#  open the raspbery pi camera in opencv
-cap = cv.VideoCapture(0)
+class camera:
+    def __init__(self):
+        '''
+            Initializes the camera
+        Args:
+            None
+        Returns:
+            None
+        '''
+        self.cap = cv2.VideoCapture(0)
+    
+    def getFrame(self):
+        '''
+            Gets a frame from the camera
+            Args:
+                None
+            Returns:
+                frame: the frame from the camera'''
+        ret, frame = self.cap.read()
+        return frame
 
-#  set the resolution of the camera to 640x480
-cap.set(3, 640)
-cap.set(4, 480)
+    def close(self):
+        '''
+            Closes the camera
+            Args:
+                None
+            Returns:
+                None
+                '''
+        self.cap.release()
 
-# open the camera in a window for 30 seconds
-while True:
-    ret, frame = cap.read()
-    cv.imshow('frame', frame)
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
 
-
-cap.release()
-cv.destroyAllWindows()
+if __name__ == "__main__":
+    cam = camera()
+    while True:
+        frame = cam.getFrame()
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cam.close()
+    cv2.destroyAllWindows()
--- hw-controls-database/ledIndicator.py
+++ hw-controls-database/ledIndicator.py
@@ -1,4 +1,5 @@
 from rpi_ws281x import PixelStrip, Color
+import time
 # LED strip configuration:
 LED_COUNT = 120        # Number of LED pixels.
 LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
@@ -7,6 +8,16 @@ LED_DMA = 10          # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False    # True to invert the signal
 LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+colours = {
+    "red": Color(255, 0, 0),
+    "green": Color(0, 255, 0),
+    "blue": Color(0, 0, 255),
+    "yellow": Color(255, 255, 0),
+    "purple": Color(255, 0, 255),
+    "cyan": Color(0, 255, 255),
+    "white": Color(255, 255, 255),
+}
 
 
 class ledIndicator:
@@ -37,11 +48,12 @@ class ledIndicator:
         Returns:
             None
             '''
+        self.clearAllLeds()
         for i in range(self.ledStrip.numPixels()):
             self.ledStrip.setPixelColor(i, color)
         self.ledStrip.show()
 
-    def setLedRoation(self, color=Color(0, 0, 255), wait_ms=50, iterations=10) -> None:
+    def setLedRoation(self, wait_ms=50, iterations=10) -> None:
         '''
             Sets the color of the leds in a rotation
         Args:
@@ -49,11 +61,13 @@ class ledIndicator:
         Returns:
             None
             '''
+        self.clearAllLeds()
+        color=Color(0, 0, 255)
         for j in range(iterations):
             for q in range(3):
                 for i in range(0, self.ledStrip.numPixels(), 3):
                     self.ledStrip.setPixelColor(i + q, color)
                 self.ledStrip.show()
-                self.ledStrip.sleep(wait_ms / 1000.0)
+                time.sleep(wait_ms / 1000.0)
                 for i in range(0, self.ledStrip.numPixels(), 3):
                     self.ledStrip.setPixelColor(i + q, 0)
